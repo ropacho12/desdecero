ff8c843d40538a0fc3d50574508c82d52b214fa0
Roxana Pacho <pachuuu@live.com.ar>
2022-08-25 15:32:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Nabvar from './componentes/Navbar/Nabvar';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <Nabvar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Navbar/Nabvar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nabvar = () => {
+  return (
+    
+    <nav>
+        <div>
+            Leo Messi Store
+        </div>
+        <div>
+            <button>Argentin</button>
+            <button>Barcelona</button>
+            <button>Psg</button>
+        </div>
+    </nav>
+ 
+
+  )
+}
+
+export default Nabvar
